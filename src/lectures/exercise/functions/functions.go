bae512a4ee95097d77a4ffee4a9f35b60dce3fe3
Yatharth Sharma <yatharth@leegality.com>
2024-04-14 07:27:34 +0530

    Added exercise for functions

--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -19,6 +19,45 @@ package main
 
 import "fmt"
 
-func main() {
+//   - Write a function that accepts a person's name as a function
+//     parameter and displays a greeting to that person.
+func greet(name string) {
+	fmt.Println("Welcome", name)
+}
 
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+func greetReturn(name string) string {
+	return "Welcome " + name
+}
+
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func sumOfThree(a, b, c int) int {
+	return a + b + c
+}
+
+// * Write a function that returns any number
+func random() int {
+	return 5
+}
+
+// * Write a function that returns any two numbers
+func returnTwo() (int, int) {
+	return 5, 8
+}
+
+//* Add three numbers together using any combination of the existing functions.
+//  * Print the result
+//* Call every function at least once
+
+func main() {
+	greet("Yatharth")
+	fmt.Println("Hey There,", greetReturn("Yatharth"))
+	fmt.Println("Sum of digits is:", sumOfThree(10, 12, 24))
+	a := random()
+	fmt.Println("Returning number", a)
+	b, c := returnTwo()
+	fmt.Println("Returning two numbers:", b, c)
+	fmt.Println("Sum of digits is:", sumOfThree(random(), b, c))
 }
